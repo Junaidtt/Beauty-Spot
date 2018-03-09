e30710266d21b23f5cae7701ad090343f3e7d6c9
Junaid <bespokino@apple-macintosh.local>
2018-03-09 17:07:06 +0530

    Initial Commit

---
+++ Beauty Spot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Beauty Spot
+//
+//  Created by Bespokino on 9/3/2561 BE.
+//  Copyright © 2561 Creamery Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
